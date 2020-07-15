b05f8d6b31f8afaa7d1dded5e5d1275b7c616639
danielczech <daniel.czech@protonmail.com>
2020-07-15 16:25:44 +0200

    Add logs from separate subscription script.

--- scripts/compare_sensors.py
+++ scripts/compare_sensors.py
@@ -7,6 +7,24 @@ Intended for debugging purposes.
 See also scrape_history_from_bluse_logs.rb - used to scrape 
 logs for the antennas and target sensors and save their history
 to Redis.
+
+Potential sensor log sources:
+
+    SENSOR_FILE    .csv downloaded from sensor history (from
+                   portal.mkat.karoo.kat.ac.za)
+
+    LOG_FILE       Derived from BLUSE katportal_server logs.
+
+    PR_LOG_FILE    Schedule block progress log file (also 
+                   from portal.mkat.karoo.kat.ac.za)
+
+    ASYNC_FILE     Logs from independently running script
+                   that connects to katportalclient separately
+                   for debugging purposes.
+                   (scripts/subscribe_async.py)
+
+    MON_FILE       Logs from independent process that also
+                   connects to katportalclient.
 """
 
 from datetime import datetime, timedelta
@@ -16,10 +34,12 @@ import pickle
 import numpy as np
 import ast
 
-SENSOR_FILE = 'subarray_1_observation_activity_A.csv' 
-#SENSOR_FILE = '/home/danielc/sensor_file_A.csv'
-LOG_FILE = '/var/log/bluse/katportal/katportal.err'
-PROGRESS_LOG_FILE = 'track_logs_2020-06-19.log'
+SENSOR_FILE = 'subarray_1_observation_activity.csv' 
+LOG_FILE = 'katportal.txt'
+PROGRESS_LOG_FILE = 'sb_joined.log'
+MON_FILE = 'mon_logs.txt'
+ASYNC_FILE = 'subscribe_async.txt'
+
 KEYSTRING = 'observation_activity'
 
 # Format matplotlib fonts:
@@ -28,11 +48,12 @@ font = {'family' : 'normal',
         'size'   : 22}
 mpl.rc('font', **font)
 
-def scrape_log(logfile, keystring):
-    """Retrieve timestamps and values for a log entry containing 
+def scrape_katportal_log(logfile, keystring):
+    """Retrieve timestamps and values for a katportal log entry containing 
        a particular string.
 
-       Example of a logfile line:
+       Examples of logfile lines:
+
            [2020-06-19 19:31:43,347 - INFO - katportal_server.py:108] 
            {'msg_channel': 'array_1:subarray_1_observation_activity', 
            'msg_pattern': 'array_1:*', 
@@ -41,11 +62,24 @@ def scrape_log(logfile, keystring):
            'name': 'subarray_1_observation_activity', 
            'timestamp': 1592587903.255768}}
 
+           [2020-07-10 15:26:08,522 - DEBUG - client.py:640] 
+           Message received: 
+           {'id': 'redis-pubsub', 
+           'result': {'msg_data': {
+           'received_timestamp': 1594387568.351311, 
+           'name': 'subarray_1_observation_activity', 
+           'timestamp': 1594387568.349612, 
+           'value': 'stop', 
+           'status': 'nominal'}, 
+           'msg_pattern': 'array_1_cac664b8-ec46-4182-945b-a54652f07fda:*', 
+           'msg_channel': 
+           'array_1_cac664b8-ec46-4182-945b-a54652f07fda:subarray_1_observation_activity'}}
+
        Searches specifically for entries which contain sensor 
        values as well (containing the string: '-->')
 
        Args:
-           logfile (str): Path to katportal_server logfile.
+           logfile (str): Path to logfile.
            keystring (str): String which identifies which line 
            to retrieve.
 
@@ -53,7 +87,6 @@ def scrape_log(logfile, keystring):
            values (list): List of extracted sensor values.
            times (list): List of timestamps (datetime objects).
     """
-    
     values = []
     times = []
     with open(logfile, 'r') as f:
@@ -61,7 +94,7 @@ def scrape_log(logfile, keystring):
             # Make sure we are getting the correct log message. 
             # Again highly specific to the BLUSE logging formats.
             if((keystring in line) & 
-                ('katportal_server.py:108' in line)):
+                ('client.py:640' in line)):
                 try:
                     time, value = extract_log_line(line)
                     values.append(value)
@@ -70,8 +103,39 @@ def scrape_log(logfile, keystring):
                     # If accidentally got a malformed or incorrect log 
                     # message, continue to the next valid line. 
                     continue
-    
     return(values, times)
+
+def scrape_async_log(logfile):
+    """Extract time and state of a single sensor from the async logs 
+    (from scripts/subscribe_async.py).
+
+    Example of a log entry:
+        [2020-07-10 13:44:14,258 - INFO - subscribe_async.py:70] 
+        subarray_1_observation_activity:track
+
+    Args:
+        logfile (str): path to logfile.
+
+    Returns:
+        values (list): List of extracted sensor values.
+        times (list): List of timestamps (datetime objects). 
+    """
+    values = []
+    times = []
+    with open(logfile, 'r') as f:
+        for line in f:
+            value = line.rsplit(':', 1)[1].strip()
+            # Get timestring:
+            time_info = line.split(']')[0]
+            time_info = time_info.split(' - ')[0]
+            # Remove leading '['
+            timestring = time_info[1:]
+            time = datetime.strptime(timestring, '%Y-%m-%d %H:%M:%S,%f')
+            # Change to UTC:
+            time = time - timedelta(hours=2)
+            values.append(value)
+            times.append(time)
+    return(values, times)         
 
 def extract_log_line(line):
     """Extract the date, time and sensor value from a particular
@@ -86,21 +150,24 @@ def extract_log_line(line):
            time (datetime object)
            sensor_value (str): sensor value extracted from the line.
 
-    """
-    
+    """   
     # Get the stringified dictionary portion of the log line:
     sensor_info = line.split(']')[1].strip()
-    sensor_info = ast.literal_eval(sensor_info)["msg_data"]
-    sensor_value = sensor_info["value"]
-    # Get the date and time:
-    timestring = line.split(' - ')[0]
-    # Remove leading '['.
-    timestring = timestring[1:]
-    time = datetime.strptime(timestring, '%Y-%m-%d %H:%M:%S,%f')
-    # Change to UTC:
-    time = time - timedelta(hours=2)
-    
-    return(time, sensor_value)
+    sensor_info = sensor_info.split(':', 1)[1].strip()
+    sensor_info = "{}".format(sensor_info)
+    try:
+        sensor_info = ast.literal_eval(sensor_info)
+        sensor_value = sensor_info["result"]["msg_data"]["value"]
+        # Get the date and time:
+        timestring = line.split(' - ')[0]
+        # Remove leading '['.
+        timestring = timestring[1:]
+        time = datetime.strptime(timestring, '%Y-%m-%d %H:%M:%S,%f')
+        # Change to UTC:
+        time = time - timedelta(hours=2)
+        return(time, sensor_value)
+    except:
+        print("Could not read line")
 
 def read_sensor_csv(sensor_file):
     """Read in a sensor .csv file from portal.mkat.karoo.kat.ac.za
@@ -115,7 +182,6 @@ def read_sensor_csv(sensor_file):
            times (list): List of datetime objects.
            values (list): List of sensor values.
     """
-    
     values = []
     times = []
     with open(sensor_file, 'r') as f:
@@ -127,16 +193,18 @@ def read_sensor_csv(sensor_file):
             time = datetime.strptime(entry[0],  '%Y-%m-%d %H:%M:%S')
             values.append(value)
             times.append(time)
-    
     return(values, times)
 
 def read_track_logs(logfile):
     """Read in progress logs from katgui and format for plotting.
        Logs are split across two lines as follows:
 
-       Example of a file entry:
-           Test 1 sb0027.log 2020-06-19 14:56:36.004Z INFO     tracking target
-           Test 1 sb0027.log 2020-06-19 14:56:36.005Z INFO     target tracked for 0.0 seconds
+       Examples of file entries:
+           2020-06-19 14:56:36.004Z INFO     tracking target
+           2020-06-19 14:56:36.005Z INFO     target tracked for 0.0 seconds
+
+       Observations can also be terminated as follows:
+           2020-07-10 13:25:58.253Z WARNING  Session interrupted by exception (KeyboardInterrupt)
 
        Args:
            logfile (str): Filepath to sensor log file.
@@ -145,7 +213,6 @@ def read_track_logs(logfile):
            times (list): List of datetime objects.
            values (list): List of sensor values.
     """
-    
     values = []
     times = []
     with open(logfile, 'r') as f:
@@ -155,24 +222,21 @@ def read_track_logs(logfile):
             if('tracking target' in line):
                 # Find start time of initiating the track:
                 entry = line.split(' ')
-                timestring = '{} {}'.format(entry[3], entry[4][:-1])
+                timestring = '{} {}'.format(entry[0], entry[1][:-1])
                 time = datetime.strptime(timestring, '%Y-%m-%d %H:%M:%S.%f')
+                values.append('track')
+                times.append(time)
                 # Find the length of the track by looking at the next line:
                 next_line = f.readline()
                 next_entry = next_line.split(' ')
-                duration = next_entry[-2]
-                # If track of length 0, skip
-                if(float(duration) == 0):
-                    continue
-                values.append('track')
-                times.append(time)
-                # Now, after duration, set to 'unknown'?
-                times.append(time + timedelta(0, float(duration)))
+                stop_timestring = '{} {}'.format(next_entry[0], next_entry[1][:-1])
+                stop_time = datetime.strptime(stop_timestring, '%Y-%m-%d %H:%M:%S.%f')
+                # Now, upon stopping, set to 'unknown'?
+                times.append(stop_time)
                 values.append('unknown')
-    
     return(values, times)
 
-def plot_values(s_vals, s_times, l_vals, l_times, pr_vals, pr_times):
+def plot_values(s_vals, s_times, l_vals, l_times, pr_vals, pr_times, a_vals, a_times):
     """Plot the values against one another with timestamps.
 
        Args:
@@ -184,6 +248,8 @@ def plot_values(s_vals, s_times, l_vals, l_times, pr_vals, pr_times):
            pr_vals (list): List of progress log sensor values 
            (from KATGUI).
            pr_times (list): List of associated datetime objects.  
+           a_vals (list): List of async script sensor values.
+           a_times (list): List of associated datetime objects.
 
        Returns:
            None
@@ -196,7 +262,6 @@ def plot_values(s_vals, s_times, l_vals, l_times, pr_vals, pr_times):
 
     # log dates to get ranges for plotting format.
     l_dates = mpl.dates.date2num(l_times)
-
     # It appears that in order to set the correct order of ytick 
     # items, one must actually plot and remove something to 'force'  
     # the correct order!
@@ -219,6 +284,11 @@ def plot_values(s_vals, s_times, l_vals, l_times, pr_vals, pr_times):
     plt.step(l_dates, l_vals, label = 'BLUSE (received)', 
         linestyle = '-.', color = 'k', where = 'post')
 
+    # Sensor values as received by the async script:
+    a_dates = mpl.dates.date2num(a_times)
+    plt.step(a_dates, a_vals, label = 'Async (received)', 
+        linestyle = '--', color = 'r', where = 'post')
+
     # Find all possible sensor states
     states = set(s_vals).union(set(l_vals)).union(set(pr_vals))
     print('States: {}'.format(states))
@@ -240,14 +310,16 @@ def plot_values(s_vals, s_times, l_vals, l_times, pr_vals, pr_times):
 if(__name__ == '__main__'):
 
     # Load logs
+    
     pr_vals, pr_times = read_track_logs(PROGRESS_LOG_FILE)
     s_vals, s_times = read_sensor_csv(SENSOR_FILE)
-    #l_vals, l_times = scrape_log(LOG_FILE, KEYSTRING)
+    l_vals, l_times = scrape_katportal_log(LOG_FILE, KEYSTRING)
+    a_vals, a_times = scrape_async_log(ASYNC_FILE)
 
     # Load pre-processed logs if need be:
-    with open('l_vals.txt', 'rb') as f:
-        l_vals = pickle.load(f)
-    with open('l_times.txt', 'rb') as f:
-        l_times = pickle.load(f)
+    # with open('l_vals.txt', 'rb') as f:
+    #     l_vals = pickle.load(f)
+    # with open('l_times.txt', 'rb') as f:
+    #     l_times = pickle.load(f)
 
-    plot_values(s_vals, s_times, l_vals, l_times, pr_vals, pr_times)
+    plot_values(s_vals, s_times, l_vals, l_times, pr_vals, pr_times, a_vals, a_times)
